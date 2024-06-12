bda5b9da4c09c45afd330730462240ba55b5f5e8
rodrigo <rodrigo.rgtic@gmail.com>
2024-06-12 10:30:30 -0300

    run notes migration

--- knexfile.js
+++ knexfile.js
@@ -1,15 +1,20 @@
-const path = require("path");
+const path = require("path")
 
 module.exports = {
   development: {
-    client: 'sqlite3',
+    client: "sqlite3",
     connection: {
-      filename: path.resolve(__dirname, "src", "database", "database.db")
+      filename: path.resolve(__dirname, "src", "database", "database.db"),
     },
     migrations: {
-      directory: path.resolve(__dirname, "src", "database", "knex", "migrations")
+      directory: path.resolve(
+        __dirname,
+        "src",
+        "database",
+        "knex",
+        "migrations"
+      ),
     },
-    useNullAsDefault: true
+    useNullAsDefault: true,
   },
-
-};
+}
